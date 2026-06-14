55b880e0a05a6d0a2713a68306dc47ef5b6c1d10
BaoNgo208 <128871855+BaoNgo208@users.noreply.github.com>
2026-06-14 20:14:42 +0700

    feat: Add solution cards's template

--- src/app/features/lading-page/pages/tiktok-ads-campain/tiktok-ads.component.ts
+++ src/app/features/lading-page/pages/tiktok-ads-campain/tiktok-ads.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero/hero.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { RedGlowCtaComponent } from '../facebook-ads-campain/components/red-glow-cta/red-glow-cta.component';
+import { HowItWorksComponent } from '../facebook-ads-campain/components/how-it-works/how-it-works.component';
+import { WhyTiktokVccComponent } from './components/why-tiktok-vcc/why-tiktok-vcc.component';
 
 @Component({
   selector: 'app-tiktok-ads',
-  imports: [HeroComponent, OverviewComponent],
+  imports: [
+    HeroComponent,
+    OverviewComponent,
+    RedGlowCtaComponent,
+    WhyTiktokVccComponent,
+    HowItWorksComponent,
+  ],
   templateUrl: './tiktok-ads.component.html',
 })
 export class TiktokAdsComponent {}
